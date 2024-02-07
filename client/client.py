c3a93bc74bc43221ffd4e88b3aadeca28ecc45a4
lruellou <louis.ruellou@etudiant.univ-rennes1.fr>
2024-02-07 12:46:31 +0100

    Reception from server in client

--- client/client.py
+++ client/client.py
@@ -3,7 +3,13 @@ from shared import network
 HOST_ADDR = ('localhost', 12345)
 
 def on_receive(data):
-    print(f'Received {data}')
+    try:
+        list_str = data.decode("utf-8").split('|')
+        list_tpl_str = [tuple(string[1:-1].split(',')) for string in list_str]
+        print(f'3: {list_tpl_str}')
+        print("\n\n")
+    except Exception as e:
+        print(f"FAILED WITH EXCEPTION : {e}")
 
 def on_remote_close():
     print('Connection closed by server')
--- server/server.py
+++ server/server.py
@@ -16,7 +16,7 @@ tasks: list[asyncio.Task] = []
 
 networks: dict[network.Address, network.Network] = {}
 
-plate = [(100000000000000000000000000000000000000000000,1,1),(2,2,2),(3,3,3),(4,4,4),(5,5,5),(6,6,6),(7,7,7),(8,8,8),(9,9,9),(10,10,10),(11,11,11),(12,12,12),(13,13,13),(14,14,14)]
+plate = [(1,1,1),(2,2,2),(3,3,3)]
 
 def on_receive(data):
     print(f'Received {data}')
